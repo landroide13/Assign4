57b3011731741f06b596db7933dcf07b5ca914f2
Luis Andrade Reuss <reysh_2@yahoo.com>
2022-04-19 11:36:25 +1200

    start

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assign4DesingPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
